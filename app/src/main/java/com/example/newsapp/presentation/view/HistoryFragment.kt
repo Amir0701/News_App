72fb135dbe8a1a64bdb2e8ad19829fef1fd454c4
Amir0701 <amirusmonov2002@gmail.com>
2024-01-18 09:14:44 +0300

    feature/search articles from history

--- app/src/main/java/com/example/newsapp/presentation/view/HistoryFragment.kt
+++ app/src/main/java/com/example/newsapp/presentation/view/HistoryFragment.kt
@@ -4,18 +4,52 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapp.R
 import com.example.newsapp.presentation.viewmodel.HistoryFragmentViewModel
+import kotlinx.coroutines.Job
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HistoryFragment : Fragment() {
     val historyFragmentViewModel: HistoryFragmentViewModel by viewModel()
     private var articlesFromHistoryRecycler: RecyclerView? = null
     private val adapter = NewsAdapter()
+    private var searchJob: Job? = null
+
+    private val menuProvider = object: MenuProvider{
+        override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+
+        }
+
+        override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+            if(menuItem.itemId == R.id.search){
+                val searchView = menuItem.actionView as SearchView?
+                searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+                    override fun onQueryTextSubmit(query: String?): Boolean {
+                        historyFragmentViewModel.isSearched = true
+                        searchJob?.cancel()
+                        searchJob = historyFragmentViewModel.searchArticles(query ?: "")
+                        return true
+                    }
+
+                    override fun onQueryTextChange(newText: String?): Boolean {
+                        return false
+                    }
+                })
+            }
+
+            return false
+        }
+
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,9 +62,17 @@ class HistoryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         (activity as  MainActivity).supportActionBar?.title = resources.getString(R.string.history_fragment_title)
         articlesFromHistoryRecycler = view.findViewById(R.id.article_history_recycler)
+        requireActivity().addMenuProvider(menuProvider)
         setUpRecycler()
         historyFragmentViewModel.getArticlesFromHistory()
         observeOnArticlesFromHistory()
+        observeOnSearchedArticles()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        requireActivity().removeMenuProvider(menuProvider)
+        searchJob?.cancel()
     }
 
     private fun setUpRecycler(){
@@ -39,8 +81,18 @@ class HistoryFragment : Fragment() {
     }
     private fun observeOnArticlesFromHistory(){
         historyFragmentViewModel.articlesInHistory.observe(viewLifecycleOwner) { articles ->
-            Log.i("size", articles.size.toString())
             adapter.setData(articles)
+        }
+    }
+
+    private fun observeOnSearchedArticles(){
+        historyFragmentViewModel.searchedArticles.observe(viewLifecycleOwner){articles->
+            articles?.let {
+                if(historyFragmentViewModel.isSearched){
+                    adapter.setData(it)
+                    historyFragmentViewModel.isSearched = false
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/newsapp/presentation/viewmodel/HistoryFragmentViewModel.kt
+++ app/src/main/java/com/example/newsapp/presentation/viewmodel/HistoryFragmentViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.newsapp.presentation.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -16,10 +17,39 @@ class HistoryFragmentViewModel(
 ): ViewModel() {
     private val _articlesInHistory = MutableLiveData<List<Article>>()
     val articlesInHistory: LiveData<List<Article>> = _articlesInHistory
-
+    var prev: LiveData<List<Article>>? = null
+    private val _searchedArticles = MutableLiveData<List<Article>>()
+    val searchedArticles: LiveData<List<Article>> = _searchedArticles
+    var isSearched = false
     fun getArticlesFromHistory() = viewModelScope.launch(Dispatchers.IO){
         val articlesFromHistory = articlesRepository.getArticlesFromHistory()
         val articles = articlesFromHistory.map(mapper::toArticle)
+        Log.i("hash code", articlesInHistory.hashCode().toString())
+        Log.i("hash code", articlesInHistory.toString())
         _articlesInHistory.postValue(articles)
+        Log.i("hash code", (articlesInHistory === prev).toString())
+
+    }
+
+    fun searchArticles(query: String) = viewModelScope.launch(Dispatchers.IO){
+        val filteredArticlesList = mutableListOf<Article>()
+        articlesInHistory.value?.let { articles ->
+            for(article in articles){
+                if(
+                    article.title.contains(query, ignoreCase = true) ||
+                    article.description.contains(query, ignoreCase = true) ||
+                    article.content.contains(query, ignoreCase = true)
+                ){
+                    filteredArticlesList.add(article)
+                }
+            }
+            _searchedArticles.postValue(filteredArticlesList)
+        }
+    }
+
+    override fun onCleared() {
+        prev = articlesInHistory
+        Log.i("hash code", "on Cleared")
+        super.onCleared()
     }
 }
