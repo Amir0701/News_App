1f0dede8aced5636f378ae6130fb031333ee643a
Amir0701 <amirusmonov2002@gmail.com>
2024-01-19 08:08:49 +0300

    recyclerview was set up

--- app/src/main/java/com/example/newsapp/presentation/view/FavoritesArticlesFragment.kt
+++ app/src/main/java/com/example/newsapp/presentation/view/FavoritesArticlesFragment.kt
@@ -6,12 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapp.R
 import com.example.newsapp.presentation.viewmodel.FavoriteFragmentViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavoritesArticlesFragment : Fragment() {
     val favoriteFragmentViewModel: FavoriteFragmentViewModel by viewModel()
+    private var recyclerView: RecyclerView? = null
+    private val adapter = NewsAdapter()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,12 +27,23 @@ class FavoritesArticlesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as MainActivity).supportActionBar?.title = resources.getString(R.string.favorites_fragment_title)
+        initViews(view)
+        setUpRecyclerView()
         observeOnFavoriteArticles()
+    }
+
+    private fun initViews(view: View){
+        recyclerView = view.findViewById(R.id.favorite_recycler)
+    }
+
+    private fun setUpRecyclerView(){
+        recyclerView?.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView?.adapter = adapter
     }
 
     private fun observeOnFavoriteArticles(){
         favoriteFragmentViewModel.favoriteArticles.observe(viewLifecycleOwner){favoriteArticles->
-            Log.i("TAG", favoriteArticles.size.toString())
+            adapter.setData(favoriteArticles)
         }
     }
 }
